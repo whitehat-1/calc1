3d630996258c2e66f49f8025025c5e61916c1587
M J <69307802+whitehat-1@users.noreply.github.com>
2022-05-10 02:56:20 -0700

    initial commit

---
+++ study.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="project.php" method="post">
+  <input type="text" name="qty" placeholder="Qty">
+  <input type="text" name="product" placeholder="Product">
+  <input type="text" name="amount" placeholder="Amount">
+  <input type="submit" name="submit" value="Submit">
+</form>
+    
+</body>
+</html>
+<html>
+ 
